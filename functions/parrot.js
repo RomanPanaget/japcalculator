da7b7ca829707622c547cbf62e2b0d490efb74be
Romain Panaget <romain.panaget@supelec.fr>
2020-10-27 09:46:20 +0100

    fix(func): change parrot.js

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,15 +1,13 @@
 exports.handler = async function (event, context) {
-    try {
-        const requestBody = JSON.parse(event.body);
-        const message = requestBody.message;
+    if (event.body) {
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: message }),
-        };
-    } catch (e) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({}),
+            body: event.body,
         };
     }
+    return {
+        statusCode: 400,
+        body: "",
+    };
+
 };
